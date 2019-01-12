3dcc28ebd8325bd0b7b24f810634d1e39aa6a47a
Gil Pratte <gil.pratte@mutualmobile.com>
2019-01-12 13:34:08 -0600

    initial commit

---
+++ src/main/java/UnitTest/App.java
@@ -0,0 +1,13 @@
+package UnitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
